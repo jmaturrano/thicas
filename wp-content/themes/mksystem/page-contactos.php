bc05921f720822176805f76ff3dfc77fa2acad55
mkdesarrollo <desarrollo@mksystemgroup.com>
2016-10-26 11:04:41 -0500

    Sincronizar

--- wp-content/themes/mksystem/page-contactos.php
+++ wp-content/themes/mksystem/page-contactos.php
@@ -1,29 +1,20 @@
 <?php
 /**
- * Template Name: Página Nosotros
+ * Template Name: Full-width(no sidebar)
  *
  * This is the template that displays full width page without sidebar
-
  *
  * @package dazzling
  */
 
 get_header(); ?>
-
 	<div id="primary" class="content-area col-sm-12 col-md-12">
-	page-nosotros
-		<main id="main" class="site-main" role="main">
-			<?php while ( have_posts() ) : the_post(); ?>
-				<?php get_template_part( 'content', 'nosotros'); ?>
-                </main>
-                    
-	<div id="primary" class="content-area col-sm-12 col-md-8">
 		<main id="main" class="site-main" role="main">
 
 			<?php while ( have_posts() ) : the_post(); ?>
 
-				<?php get_template_part( 'content', 'nosotros' ); ?>
-                                
+				<?php get_template_part( 'content', 'page' ); ?>
+
 				<?php
 					// If comments are open or we have at least one comment, load up the comment template
 					if ( comments_open() || '0' != get_comments_number() ) :
@@ -31,11 +22,9 @@ get_header(); ?>
 					endif;
 				?>
 
-
 			<?php endwhile; // end of the loop. ?>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
-      </div>
- <?php get_sidebar(); ?>
- <?php get_footer(); ?>
+
+<?php get_footer(); ?>
